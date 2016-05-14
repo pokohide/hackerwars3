0624e010925f3a6b129d8b9fab2aff7e177caa33
GuiltyMorishita <morishita@lang.cs.tut.ac.jp>
2016-05-14 15:54:32 +0900

    card model 作り直し＆dbリセット

--- db/migrate/20160514065225_create_cards.rb
+++ db/migrate/20160514065225_create_cards.rb
@@ -2,11 +2,11 @@ class CreateCards < ActiveRecord::Migration
   def change
     create_table :cards do |t|
       t.integer :user_id
-      t.string :username
-      t.string :screenname
+      t.string :name
+      t.string :screen_name
       t.integer :tweets_count
-      t.integer :follower_count
-      t.string :thumbnail
+      t.integer :followers_count
+      t.string :profile_image_url
       t.string :category
 
       t.timestamps null: false
