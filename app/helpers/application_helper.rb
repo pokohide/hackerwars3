3e51c30316ae2548941972691184bd17365ced17
hyde2able <hyde31415@gmail.com>
2016-05-14 13:42:14 +0900

    slimに変更

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+	def alert_or_notice alert, notice
+        return if !alert.present? && !notice.present?
+        if alert
+            type = "warning"
+        elsif notice
+            type = "success"
+        end
+
+        content_tag(:div, class: "alert alert-#{type}", style: "margin:10px 0px") do
+            "<a href='#' class='close' data-dismiss='alert' aria-label='close'>&times;</a>"
+            alert || notice
+        end
+    end
 end
