50b140ac298474458804db2fc1898ca0929b9cc7
GuiltyMorishita <morishita@lang.cs.tut.ac.jp>
2016-05-14 16:03:37 +0900

    お笑いカテを追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Card.create(user_id:nil, name:"有吉弘行", screen_name:"ariyoshihiroiki", tweets_count:"15229", followers_count:"5669714", profile_image_url:"http://pbs.twimg.com/profile_images/666631354566635520/Qat08REd_normal.jpg", category:"comedy")
+Card.create(user_id:nil, name:"宮迫", screen_name:"motohage", tweets_count:"9856", followers_count:"2148368", profile_image_url:"http://pbs.twimg.com/profile_images/344513261581267470/b58f2fe82ea424323033bbf97a7f3a4c_normal.jpeg", category:"comedy")
+Card.create(user_id:nil, name:"田村淳", screen_name:"atsushilonboo", tweets_count:"19125", followers_count:"1989112", profile_image_url:"http://pbs.twimg.com/profile_images/728226578493149184/G62qWH16_normal.jpg", category:"comedy")
+Card.create(user_id:nil, name:"宮川大輔", screen_name:"kesuidawagayami", tweets_count:"4940", followers_count:"1819093", profile_image_url:"http://pbs.twimg.com/profile_images/610147984995528704/1YEsBX_x_normal.jpg", category:"comedy")
+Card.create(user_id:nil, name:"徳井義実", screen_name:"tokuiyoshimi", tweets_count:"3567", followers_count:"1708107", profile_image_url:"http://pbs.twimg.com/profile_images/554439808781516801/JWn3whnG_normal.jpeg", category:"comedy")
+Card.create(user_id:nil, name:"バカリズム", screen_name:"BAKARHYTHM", tweets_count:"20000", followers_count:"1363941", profile_image_url:"http://pbs.twimg.com/profile_images/727733738075992064/vObLYdsu_normal.jpg", category:"comedy")
+Card.create(user_id:nil, name:"渡辺直美", screen_name:"watanabe_naomi", tweets_count:"12932", followers_count:"1130734", profile_image_url:"http://pbs.twimg.com/profile_images/378800000502166731/6cec8a8a52316c4e1ce585e96c975329_normal.jpeg", category:"comedy")
+Card.create(user_id:nil, name:"劇団ひとり", screen_name:"GekidanHitori", tweets_count:"2328", followers_count:"1066052", profile_image_url:"http://pbs.twimg.com/profile_images/1385434555/DSC01800__1___Large__normal.JPG", category:"comedy")
+Card.create(user_id:nil, name:"又吉直樹", screen_name:"matayoshi0", tweets_count:"402", followers_count:"1003029", profile_image_url:"http://pbs.twimg.com/profile_images/1470359416/d2e1fa1785de86ba1e35e48756af47a8_normal.jpeg", category:"comedy")
