c97dee4492a6157692025b0da792c2631c36a18b
hyde2able <hyde31415@gmail.com>
2016-05-15 01:04:36 +0900

    twitterに本番環境追加

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -37,8 +37,11 @@ Devise.setup do |config|
   # config.authentication_keys = [:email]
   config.authentication_keys = [ :username ]
 
-  config.omniauth :twitter, "8XDFefeCi9JKqW0nnJkJxV62c", "ExyBu6FMWyp49xHwZFqXeUMcepDXND6dKHl16mdKoz2qnqK5Cz"
-
+  if Rails.env.production?
+    config.omniauth :twitter, ENV['TWITTER_CONSUMER_KEY'], ENV['TWITTER_CONSUMER_SECRET']
+  else
+    config.omniauth :twitter, "8XDFefeCi9JKqW0nnJkJxV62c", "ExyBu6FMWyp49xHwZFqXeUMcepDXND6dKHl16mdKoz2qnqK5Cz"
+  end
   # Configure parameters from the request object used for authentication. Each entry
   # given should be a request method and it will automatically be passed to the
   # find_for_authentication method and considered in your model lookup. For instance,
