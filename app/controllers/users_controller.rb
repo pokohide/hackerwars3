65fd48265fd596cc1cd6857ecfdaeb33ce752dfd
hyde2able <hyde31415@gmail.com>
2016-05-14 15:58:46 +0900

    cardとuserno

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,17 @@
 class UsersController < ApplicationController
-  before_action :authenticate_user!, only: :show
+  before_action :authenticate_user!, only: [:show, :get_cards]
   def index
   end
 
   def show
+  	unless current_user.cards.present?
+  		redirect_to :get_cards
+  		return
+  	end
+
+  end
+
+  def get_cards
+
   end
 end
